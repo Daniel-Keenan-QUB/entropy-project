de2c11a3c725b0b8b0bde6e11882180c45783cbd
Daniel Keenan <dkeenan21@qub.ac.uk>
2021-07-09 04:22:07 +0100

    Add CI pipeline configuration

---
+++ src/test/java/uk/ac/qub/dkeenan21/SampleTest.java
@@ -0,0 +1,17 @@
+package uk.ac.qub.dkeenan21;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+	@Test
+	public void testAddition() {
+		assertEquals(12 + 25, 37);
+	}
+
+	@Test
+	public void testSubtraction() {
+		assertEquals(32 - 17, 15);
+	}
+}
