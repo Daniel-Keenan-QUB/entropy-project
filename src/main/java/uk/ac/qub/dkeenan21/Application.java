1c53c09fadd60208281b8b3bc160c123afe4842c
Daniel Keenan <dkeenan21@qub.ac.uk>
2021-07-16 18:34:29 +0100

    Implement CLI

--- src/main/java/uk/ac/qub/dkeenan21/Application.java
+++ src/main/java/uk/ac/qub/dkeenan21/Application.java
@@ -1,18 +1,71 @@
 package uk.ac.qub.dkeenan21;
 
+import org.apache.commons.cli.*;
 import org.apache.log4j.varia.NullAppender;
+import org.tinylog.Logger;
 import uk.ac.qub.dkeenan21.driver.AnalysisDriver;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import static org.apache.log4j.LogManager.getRootLogger;
 
 public class Application {
-	private static final String repositoryPath = "../refactoring-toy-example";
-	private static final String[] fileTypes = new String[]{"java"};
-
 	public static void main(String[] args) {
-		getRootLogger().addAppender(new NullAppender()); // disable log4j output from JGit library
-		new AnalysisDriver(repositoryPath).analyse(1, Set.of(fileTypes));
+		final Options options = configureCommandLineOptions();
+		try {
+			final CommandLine commandLine = new DefaultParser().parse(options, args);
+			final String repositoryPath = commandLine.getOptionValue("repository-path");
+			final int changePeriodSize = Integer.parseInt(commandLine.getOptionValue("change-period-size"));
+			final String[] fileTypeWhitelist = commandLine.getOptionValue("file-type-whitelist").split(" ");
+
+			Logger.info("Beginning analysis with program arguments");
+			Logger.info("— Repository path: " + repositoryPath);
+			Logger.info("— Change period size: " + changePeriodSize);
+			Logger.info("— File type whitelist = " + Arrays.toString(fileTypeWhitelist));
+
+			getRootLogger().addAppender(new NullAppender()); // disable log4j output from JGit library
+			final AnalysisDriver analysisDriver = new AnalysisDriver(repositoryPath);
+			analysisDriver.analyse(changePeriodSize, Set.of(fileTypeWhitelist));
+		} catch (ParseException parseException) {
+			// incorrect use of command-line options, so print usage message and exit with non-zero status code
+			final HelpFormatter helpFormatter = new HelpFormatter();
+			helpFormatter.printHelp("repository-analyser", options);
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * Configure the command-line options to be accepted by the application
+	 *
+	 * @return the command-line options
+	 */
+	private static Options configureCommandLineOptions() {
+		final Option repositoryPathOption = Option.builder("rp")
+				.longOpt("repository-path")
+				.argName("PATH")
+				.desc("analyse the Git repository at PATH")
+				.hasArg()
+				.required()
+				.build();
+		final Option changePeriodSizeOption = Option.builder("sz")
+				.longOpt("change-period-size")
+				.argName("SIZE")
+				.desc("define a change period as SIZE commits")
+				.hasArg()
+				.required()
+				.build();
+		final Option fileTypeWhiteListOption = Option.builder("wl")
+				.longOpt("file-type-whitelist")
+				.argName("WHITELIST")
+				.desc("only consider the file type extensions specified in space-separated list WHITELIST")
+				.hasArgs() // sets that number of arguments is unlimited
+				.required()
+				.build();
+		final Options options = new Options();
+		options.addOption(repositoryPathOption);
+		options.addOption(changePeriodSizeOption);
+		options.addOption(fileTypeWhiteListOption);
+		return options;
 	}
 }
